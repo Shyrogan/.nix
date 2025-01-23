de374092b408f950515d8770e85715cb3e3dc305
Sébastien <sebastien@shyrogan.fr>
2025-01-23 17:51:45 +0100

    feat: rust

--- home/modules/programming.nix
+++ home/modules/programming.nix
@@ -1,8 +1,13 @@
-{ lib, ... }: with lib; {
+{ lib, config, pkgs, ... }: with lib; let
+  rust = config.programming.rust;
+  web = config.programming.web;
+in
+{
   options = {
     programming = {
       rust.enable = mkEnableOption "Enable Rust programming.";
       web.enable = mkEnableOption "Enable Node programming.";
     };
   };
+  config.home.packages = with pkgs; optionals rust.enable [ cargo rustc clippy rust-analyzer ];
 }
