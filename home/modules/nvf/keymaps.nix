476ccbbea70f06448f992169c3e41a1083730b09
Sébastien <sebastien@shyrogan.fr>
2025-04-28 10:20:54 +0200

    ref: nvf config

---
+++ home/modules/nvf/keymaps.nix
@@ -0,0 +1,32 @@
+[
+  {
+    key = "<C-s>";
+    action = "<cmd>w<cr>";
+    mode = ["n" "v" "x"];
+    desc = "Save buffer";
+  }
+  {
+    key = "<C-j>";
+    action = "<cmd>wincmd j<cr>";
+    mode = ["n" "v" "x"];
+    desc = "Move to pane below";
+  }
+  {
+    key = "<C-k>";
+    action = "<cmd>wincmd k<cr>";
+    mode = ["n" "v" "x"];
+    desc = "Move to pane above";
+  }
+  {
+    key = "<C-l>";
+    action = "<cmd>wincmd l<cr>";
+    mode = ["n" "v" "x"];
+    desc = "Move to pane to the right";
+  }
+  {
+    key = "<C-h>";
+    action = "<cmd>wincmd h<cr>";
+    mode = ["n" "v" "x"];
+    desc = "Move to pane to the left";
+  }
+]
---
+++ home/modules/nvf/options.nix
@@ -0,0 +1,33 @@
+{
+  # Set tabs to 2 spaces
+  tabstop = 2;
+  softtabstop = 2;
+  showtabline = 2;
+  expandtab = true;
+
+  # Enable auto indenting and set it to spaces
+  smartindent = true;
+  shiftwidth = 2;
+
+  # Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
+  breakindent = true;
+
+  # Enable incremental searching
+  hlsearch = true;
+  incsearch = true;
+
+  # Enable ignorecase + smartcase for better searching
+  ignorecase = true;
+  smartcase = true; # Don't ignore case with capitals
+  grepprg = "rg --vimgrep";
+  grepformat = "%f:%l:%c:%m";
+
+  # Decrease updatetime
+  updatetime = 50; # faster completion (4000ms default)
+
+  # Enable text wrap
+  wrap = true;
+
+  list = true; # Show invisible characters (tabs, eol, ...)
+  listchars = "eol:↲,tab:··,lead:·,space: ,trail:•,extends:→,precedes:←,nbsp:␣";
+}
