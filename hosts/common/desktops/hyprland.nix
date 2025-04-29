b9a89d67516caed74e28a916b23162f3dd5ee499
Sébastien <sebastien@shyrogan.fr>
2025-04-29 11:55:59 +0200

    chore: disable greetd to manually login

--- hosts/common/desktops/hyprland.nix
+++ hosts/common/desktops/hyprland.nix
@@ -1,4 +1,11 @@
-{ inputs, pkgs, lib, config, ... }: with lib; let
+{
+  inputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib; let
   inherit (inputs) hyprland;
   cfg = config.programs.hyprland;
   hyprPkgs = hyprland.packages.${pkgs.system};
@@ -11,22 +18,22 @@ in {
     };
 
     services = {
-      greetd = {
-        enable = true;
-        settings = rec {
-          default_session = {
-            command = "${programs.hyprland.package}/bin/Hyprland";
-            user = "sebastien";
-          };
-          initial_session = default_session;
-        };
-      };
+      # greetd = {
+      #   enable = true;
+      #   settings = rec {
+      #     default_session = {
+      #       command = "${programs.hyprland.package}/bin/Hyprland";
+      #       user = "sebastien";
+      #     };
+      #     initial_session = default_session;
+      #   };
+      # };
       gvfs.enable = true;
       upower.enable = true;
       power-profiles-daemon.enable = true;
       accounts-daemon.enable = true;
     };
-    
+
     environment.systemPackages = with pkgs; [
       adwaita-icon-theme
       morewaita-icon-theme
@@ -41,9 +48,9 @@ in {
 
     systemd.user.services.polkit-gnome-authentication-agent-1 = {
       description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
       serviceConfig = {
         Type = "simple";
         ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
