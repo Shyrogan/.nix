0890f751201db5ddbb189fb18b3bd679a01c10ad
Sébastien VIAL <sebastien@shyrogan.fr>
2025-09-03 20:31:44 +0200

    feat: niri, steam installed with nixos

--- configurations/nixos/kazai/configuration.nix
+++ configurations/nixos/kazai/configuration.nix
@@ -1,9 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -53,10 +48,17 @@
   users.users.sebastien = {
     isNormalUser = true;
     description = "sebastien";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    shell = pkgs.nushell;
   };
 
   programs.git.enable = true;
+  programs.niri.enable = true;
+  programs.steam.enable = true;
+  environment.systemPackages = [ pkgs.xwayland-satellite ];
 
   nixpkgs.config.allowUnfree = true;
 
--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -1,12 +1,17 @@
-{ config, flake, ... }: 
-let
-  inherit (flake) inputs;
-  inherit (inputs) niri;
-in
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   programs.niri = {
     settings = {
       environment."NIXOS_OZONE_WL" = "1";
+      xwayland-satellite = {
+        enable = true;
+        path = lib.getExe pkgs.xwayland-satellite-unstable;
+      };
 
       input = {
         keyboard = {
@@ -14,64 +19,108 @@ in
             layout = "fr";
           };
         };
+        mouse = {
+          accel-speed = 0.4;
+        };
       };
-      
+
       layout = {
         preset-column-widths = [
-	  { proportion = 1. / 3.; }
-	  { proportion = 1. / 2.; }
-	  { proportion = 2. / 3.; }
-	  { proportion = 1.; }
-	];
+          { proportion = 1.0 / 3.0; }
+          { proportion = 1.0 / 2.0; }
+          { proportion = 2.0 / 3.0; }
+          { proportion = 1.0; }
+        ];
         default-column-width = {
-	  proportion = 1.0;
-	};
+          proportion = 1.0;
+        };
       };
+
+      window-rules = [
+        {
+          geometry-corner-radius =
+            let
+              radius = 12.0;
+            in
+            {
+              bottom-left = radius;
+              bottom-right = radius;
+              top-left = radius;
+              top-right = radius;
+            };
+          clip-to-geometry = true;
+        }
+        {
+          matches = [ { app-id = "zen"; } ];
+          open-on-workspace = "03-browser";
+        }
+        {
+          matches = [ { app-id = "vesktop"; } ];
+          open-on-workspace = "09-discord";
+        }
+        {
+          matches = [ { app-id = "10-spotify"; } ];
+          open-on-workspace = "10-spotify";
+        }
+      ];
 
       binds = with config.lib.niri.actions; {
         "Mod+X".action = close-window;
-	"Mod+O".action = toggle-overview;
-	"Mod+R".action = switch-preset-window-width;
-	"Mod+T".action = switch-preset-window-height;
-	"Mod+F".action = fullscreen-window;
+        "Mod+O".action = toggle-overview;
+        "Mod+R".action = switch-preset-window-width;
+        "Mod+T".action = switch-preset-window-height;
+        "Mod+F".action = fullscreen-window;
 
         # A few quick programs required all the time
         "Mod+Return".action.spawn = "ghostty";
         "Mod+Space".action.spawn = "fuzzel";
 
         # Windows + columns
-	"Mod+H".action = focus-column-left;
-	"Mod+J".action = focus-window-down;
-	"Mod+K".action = focus-window-up;
-	"Mod+L".action = focus-column-right;
-	"Mod+Shift+H".action = move-column-left;
-	"Mod+Shift+J".action = move-window-down;
-	"Mod+Shift+K".action = move-window-up;
-	"Mod+Shift+L".action = move-column-right;
+        "Mod+H".action = focus-column-left;
+        "Mod+J".action = focus-window-down;
+        "Mod+K".action = focus-window-up;
+        "Mod+L".action = focus-column-right;
+        "Mod+Shift+H".action = move-column-left;
+        "Mod+Shift+J".action = move-window-down;
+        "Mod+Shift+K".action = move-window-up;
+        "Mod+Shift+L".action = move-column-right;
 
-	# Workspaces (TODO: refactor with loops)
-	"Mod+Ampersand".action = focus-workspace 1;
-	"Mod+Eacute".action = focus-workspace 2;
-	"Mod+Quotedbl".action = focus-workspace 3;
-	"Mod+Apostrophe".action = focus-workspace 4;
-	"Mod+Parenleft".action = focus-workspace 5;
-	"Mod+Minus".action = focus-workspace 6;
-	"Mod+Egrave".action = focus-workspace 7;
-	"Mod+Underscore".action = focus-workspace 8;
-	"Mod+Ccedilla".action = focus-workspace 9;
-	"Mod+Agrave".action = focus-workspace 10;
-	"Mod+Shift+Ampersand".action.move-column-to-workspace = 1;
-	"Mod+Shift+Eacute".action.move-column-to-workspace = 2;
-	"Mod+Shift+Quotedbl".action.move-column-to-workspace = 3;
-	"Mod+Shift+Apostrophe".action.move-column-to-workspace = 4;
-	"Mod+Shift+Parenleft".action.move-column-to-workspace = 5;
-	"Mod+Shift+Minus".action.move-column-to-workspace = 6;
-	"Mod+Shift+Egrave".action.move-column-to-workspace = 7;
-	"Mod+Shift+Underscore".action.move-column-to-workspace = 8;
-	"Mod+Shift+Ccedilla".action.move-column-to-workspace = 9;
-	"Mod+Shift+Agrave".action.move-column-to-workspace = 10;
+        # Workspaces (TODO: refactor with loops)
+        "Mod+Ampersand".action = focus-workspace "01-main-workspace";
+        "Mod+Eacute".action = focus-workspace "02-secondary-workspace";
+        "Mod+Quotedbl".action = focus-workspace "03-browser";
+        "Mod+Apostrophe".action = focus-workspace 4;
+        "Mod+Parenleft".action = focus-workspace 5;
+        "Mod+Minus".action = focus-workspace 6;
+        "Mod+Egrave".action = focus-workspace 7;
+        "Mod+Underscore".action = focus-workspace 8;
+        "Mod+Ccedilla".action = focus-workspace "09-discord";
+        "Mod+Agrave".action = focus-workspace "10-spotify";
+        "Mod+Shift+Ampersand".action.move-column-to-workspace = "01-main-workspace";
+        "Mod+Shift+Eacute".action.move-column-to-workspace = "02-secondary-workspace";
+        "Mod+Shift+Quotedbl".action.move-column-to-workspace = "03-browser";
+        "Mod+Shift+Apostrophe".action.move-column-to-workspace = 4;
+        "Mod+Shift+Parenleft".action.move-column-to-workspace = 5;
+        "Mod+Shift+Minus".action.move-column-to-workspace = 6;
+        "Mod+Shift+Egrave".action.move-column-to-workspace = 7;
+        "Mod+Shift+Underscore".action.move-column-to-workspace = 8;
+        "Mod+Shift+Ccedilla".action.move-column-to-workspace = "09-discord";
+        "Mod+Shift+Agrave".action.move-column-to-workspace = "10-spotify";
 
-	"Mod+V".action = toggle-window-floating;
+        "Mod+V".action = toggle-window-floating;
+      };
+
+      workspaces = {
+        "01-main-workspace" = {
+        };
+        "02-secondary-workspace" = {
+        };
+        "03-browser" = {
+        };
+        "09-discord" = {
+        };
+        "10-spotify" = {
+        };
       };
     };
   };
