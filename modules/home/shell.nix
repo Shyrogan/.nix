3cdebb0b95edc161ce2aa49778c2a7aabc7c9e4d
Sébastien <sebastien@shyrogan.fr>
2025-12-01 10:14:35 +0100

    chore: delete exa

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -1,41 +1,35 @@
-{ config, ... }:
-{
+{config, ...}: {
   age.secrets.env.file = ../../secrets/env.age;
-  programs =
-    let
-      enabled = config.programs.nushell.enable;
-    in
-    {
-      nushell = {
-        shellAliases = {
-          "cat" = "bat";
-        };
-        settings = {
-          show_banner = false;
-        };
-        envFile.text = ''
-          source /run/user/1000/agenix/env
-        '';
+  programs = let
+    enabled = config.programs.nushell.enable;
+  in {
+    nushell = {
+      shellAliases = {
+        "cat" = "bat";
       };
-
-      bat.enable = enabled;
-
-      carapace.enable = enabled;
-      carapace.enableNushellIntegration = enabled;
-
-      eza.enable = enabled;
-      eza.enableNushellIntegration = enabled;
-
-      nix-your-shell.enable = enabled;
-      nix-your-shell.enableNushellIntegration = enabled;
-
-      pay-respects.enable = enabled;
-      pay-respects.enableNushellIntegration = enabled;
-
-      starship.enable = enabled;
-      starship.enableNushellIntegration = enabled;
-
-      zoxide.enable = enabled;
-      zoxide.enableNushellIntegration = enabled;
+      settings = {
+        show_banner = false;
+      };
+      envFile.text = ''
+        source /run/user/1000/agenix/env
+      '';
     };
+
+    bat.enable = enabled;
+
+    carapace.enable = enabled;
+    carapace.enableNushellIntegration = enabled;
+
+    nix-your-shell.enable = enabled;
+    nix-your-shell.enableNushellIntegration = enabled;
+
+    pay-respects.enable = enabled;
+    pay-respects.enableNushellIntegration = enabled;
+
+    starship.enable = enabled;
+    starship.enableNushellIntegration = enabled;
+
+    zoxide.enable = enabled;
+    zoxide.enableNushellIntegration = enabled;
+  };
 }
